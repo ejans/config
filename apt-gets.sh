af993c6cf73f2d7b92ff36691c008326e3926bf5
Evert Jans <evert@sonuwe.com>
2013-09-18 20:11:15 +0200

    added conkyrc, changed apt-gets for laptop and desktop

--- apt-gets.sh
+++ apt-gets.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# laptop
 sudo apt-get install alsa-tools conky disper eclipse eog zathura file-roller firefox geany git flashplugin-installer mercurial \
 mplayer nvidia-common nvidia-current nvidia-settings pbzip2 pm-utils rox-filer vim virtualbox wicd wicd-curses rfkill wireshark \
 xfonts-terminus* xorg lrzip
+
+# desktop
+sudo apt-get install alsa-tools conky disper eclipse eog zathura file-roller firefox geany git flashplugin-installer mercurial \
+mplayer nvidia-common nvidia-current nvidia-settings pbzip2 rox-filer vim virtualbox wireshark \
+xfonts-terminus* xorg
 
 sudo apt-get build-dep dwm
 
@@ -12,6 +18,12 @@ cd /lib/x86_64-linux-gnu/
 
 sudo apt-get install libxss1
 sudo ln -s libudev.so.0 libudev.so.1
+
+# Iron dependents
+#sudo apt-get install libxss1
+#wget -c www.mirrorservice.org/sites/archive.ubuntu.com/ubuntu//pool/main/u/udev/libudev0_175-0ubuntu13_amd64.deb
+#sudo dpkg -i libudev0*.deb
+
 
 #sudo apt-get install alsa-tools ant-contrib apache2 arduino calibre conky disper eclipse eog evolution evince file-rottler \
 #firefox geany flashplugin-installer git gnumeric keepassx kpat latex-beamer mercurial mplayer nvidia-cg-toolkit nvidia-common \
