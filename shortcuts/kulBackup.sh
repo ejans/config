77fab7091e4e515df71cf3fc4cf2b8eb6076f551
Evert Jans <evert.jans@mech.kuleuven.be>
2013-11-04 11:14:50 +0100

    changed kulBackup with zpaq stuff

--- shortcuts/kulBackup.sh
+++ shortcuts/kulBackup.sh
@@ -8,12 +8,19 @@
 #sudo umount /media/mmcblk0p1
 #echo "done!"
 
+#cd ~/Documents
+#lrztar -l KULeuven
+#mv KULeuven.tar.lrz KULeuven_Backup_$(date +%Y_%m_%d).tar.lrz
+#echo -e "\033[37mdone compressing...\033[0m"
+#sudo mount /dev/mmcblk0p1 /media/mmcblk0p1/
+#sudo rm /media/mmcblk0p1/KULeuven/KULeuven_Backup*.tar.lrz
+#sudo cp KULeuven_Backup_$(date +%Y_%m_%d).tar.lrz /media/mmcblk0p1/KULeuven/ 
+#sudo umount /media/mmcblk0p1
+#echo -e "\033[37mdone!\033[0m"
+
 cd ~/Documents
-lrztar -l KULeuven
-mv KULeuven.tar.lrz KULeuven_Backup_$(date +%Y_%m_%d).tar.lrz
+sudo mount -o umask=0 /dev/mmcblk0p1 /media/mmcblk0p1/
+zpaq a /media/mmcblk0p1/KULeuven/KULeuven_Backup.zpaq KULeuven/
 echo -e "\033[37mdone compressing...\033[0m"
-sudo mount /dev/mmcblk0p1 /media/mmcblk0p1/
-sudo rm /media/mmcblk0p1/KULeuven/KULeuven_Backup*.tar.lrz
-sudo cp KULeuven_Backup_$(date +%Y_%m_%d).tar.lrz /media/mmcblk0p1/KULeuven/ 
-sudo umount /media/mmcblk0p1
+sudo umount /media/mmcblk0p1/
 echo -e "\033[37mdone!\033[0m"
