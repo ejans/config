d2f09b071eff04b69bdd31a3d767f6c4b9cefee2
Evert Jans <evert.jans@mech.kuleuven.be>
2014-06-10 14:14:52 +0200

    changed copy to link for dotfiles

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-# Copy dotfiles, screenfetch and shortcuts folders
-cd ~/Documents/config
-cp dotfiles/.* ~/
+# link dotfiles, screenfetch and shortcuts folders
+cd ~/
+ln -sv Documents/config/dotfiles/.bashrc
+ln -sv Documents/config/dotfiles/.bash_aliases
+ln -sv Documents/config/dotfiles/.bash_functions
+ln -sv Documents/config/dotfiles/.conkyrc
+ln -sv Documents/config/dotfiles/.config
+ln -sv Documents/config/dotfiles/.gitconfig
+ln -sv Documents/config/dotfiles/.mcabber
+ln -sv Documents/config/dotfiles/.mplayer
+ln -sv Documents/config/dotfiles/.profile
+ln -sv Documents/config/dotfiles/.vimrc
+ln -sv Documents/config/dotfiles/.Xdefaults
+ln -sv Documents/config/dotfiles/.xinitrc
 # give .viminfo right user to use previous mark position
 sudo rm -fv ~/.viminfo
 touch ~/.viminfo
-cp -frv screenfetch ~/Documents/Source/
-cp -frv shortcuts/ ~/Documents/.Shortcuts/
+#cp -frv screenfetch ~/Documents/Source/
+ln -sv screenfetch Documents/Source/screenfetch 
+#cp -frv shortcuts/ ~/Documents/.Shortcuts/
+ln -sv shortcuts Documents/.Shortcuts/
 
 cd files_to_copy/
 
 # Copy custom xorg configuration
-sudo cp -frv xorg.conf /etc/X11/
+#sudo cp -frv xorg.conf /etc/X11/
 
 # Link binary files
-sudo ln -vs ~/Documents/Source/zpaq/zpaq /usr/local/bin/zpaq
+sudo ln -sv ~/Documents/Source/zpaq/zpaq /usr/local/bin/zpaq
 
 # Replace original failsafe to shorten boot time
 sudo cp -frv failsafe.conf /etc/init/
@@ -24,5 +37,5 @@ sudo cp -frv failsafe.conf /etc/init/
 sudo cp -frv us /usr/share/X11/xkb/symbols/
 
 # Move eduroam template to wicd and add to active templates
-sudo cp -frv eduroam /etc/wicd/encryption/templates/
-sudo echo "eduroam" >> /etc/wicd/encryption/active
+#sudo cp -frv eduroam /etc/wicd/encryption/templates/
+#sudo echo "eduroam" >> /etc/wicd/encryption/active
